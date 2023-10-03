5fb64214b58fa7c6aabf8293efe9a5de5670573f
manichandra-donthula <mrmanichandra222@gmail.com>
2023-10-03 23:53:38 +0530

    day5

--- HEPST/day-5/nxn_magic_square.py
+++ HEPST/day-5/nxn_magic_square.py
@@ -1,20 +1,22 @@
-def generate_magic_square(n):
-    magic_square = [[0] * n for x in range(n)]
+def generateMagicSquare(n):
+    magicSquare = [[0] * n for x in range(n)]
     row, col = 0, n // 2
     num = 1
-    while (num <= n * n):
-        magic_square[row][col] = num
+    while (num <= (n * n)):
+        magicSquare[row][col] = num
         num += 1
-        row, col = (row - 1) % n, (col + 1) % n
-        if magic_square[row][col] != 0:
+        new_row, new_col = (row - 1) % n, (col + 1) % n
+        if (magicSquare[new_row][new_col]):
             row = (row + 1) % n
-    return magic_square
+        else:
+            row, col = new_row, new_col
+    return magicSquare
 
 n = int(input("Enter the order of the magic square that must be odd: "))
 if (n % 2 == 0):
     print("The order of the magic square must be odd.")
 else:
-    magic_square = generate_magic_square(n)
+    magicSquare = generateMagicSquare(n)
     print(f"Magic Square of order {n}:")
-    for i in magic_square:
+    for i in magicSquare:
         print(*i)
