4bcbfbf3916d30949c5ba2d54d30bec6863592a7
manichandra-donthula <mrmanichandra222@gmail.com>
2024-06-07 11:39:47 +0530

    stack implementation

---
+++ Placement Training/stack.py
@@ -0,0 +1,31 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+    def is_empty(self):
+        return len(self.items) == 0
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Pop from an Empty Stack")
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Peek from an empty stack")
+    def size(self):
+        return len(self.items)
+
+stack= Stack()
+print("Is the stack empty?", stack.is_empty())
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print("Stack:",stack.items)
+print("Top of the stack:",stack.peek())
+print("Pop:",stack.pop())
+print("Stack after pop:",stack.items)
+print("Is the stack empty?",stack.is_empty)
+print("Size of the stack:",stack.size())
