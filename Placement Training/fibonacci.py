0b483134aa28d53613cfc5fa3d561e1fa9127cb9
manichandra-donthula <mrmanichandra222@gmail.com>
2024-06-10 14:24:46 +0530

    fibonacci.py

--- Placement Training/fibonacci.py
+++ Placement Training/fibonacci.py
@@ -1,25 +1,26 @@
-n = int(input("Enter number for fibanocci: "))
-
-a = 0
-b = 0
-c = 0
-if n == 0 or n == 1:
-        c = 0
+n = int(input("Enter number: "))
+'''# Normal approach for printing series upto Fn
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
 else:
+    a = 0
+    b = 1
+    fi = [a, b]
     for i in range(1, n):
         c = a + b
         a = b
-        b = i
-        print(c)
-# 0 1 1 2 3 5 8 13
+        b = c
+        fi.append(c)
+print(*fi)'''
 
-# Recursion
-'''def fibonacci(n):
-    n1 = 0
-    n2 = 1
+# Recursion for only printing Fn
+def fibonacci(n):
+    if n == 0:
+        return 0
     if n == 1:
-        return n
+        return 1
     else:
-        return 
-
-print(fibonacci(n))'''
+        return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(n))
---
+++ Placement Training/range&iterations.py
@@ -0,0 +1,18 @@
+l = [4, 5, 6, 7, 8, 9, 10]
+# for i in l:
+#     l.remove(i)
+for i in range(len(l)):
+    l.remove(l[0])
+    print(l)
+print(l)
+a = range(2, 5)
+print(a)
+l = [2, 3, 4, 5]
+print(l[4:7])
+for i in [2, 4, 8, 10, 12]:
+    print(i)
+    i = i + 2
+li = [2, 4, 8, 10, 12]
+for i in range(len(li)):
+    print(li[i])
+    i = i + 2
